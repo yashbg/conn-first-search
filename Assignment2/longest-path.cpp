3d9fc49c277cfbcf9a1aa12c1c6114f18e631891
Yash Gupta <yashbg7@gmail.com>
2022-05-31 21:27:34 +0530

    Add longest-path.cpp

---
+++ Assignment2/longest-path.cpp
@@ -0,0 +1,60 @@
+// Question 2
+// https://cses.fi/problemset/task/1680
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> g;
+vector<int> col, order, l_longest;
+
+bool dfs(int u){
+    col[u] = 1;
+    for(int v : g[u]){
+        if(col[v] == 1){
+            return false;
+        }
+        if(!col[v] && !dfs(v)){
+            return false;
+        }
+    }
+    col[u] = 2;
+    order.push_back(u);
+    return true;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin >> n >> m;
+    g.assign(n, vector<int>());
+    for(int i = 0; i < m; i++){
+        int a, b;
+        cin >> a >> b;
+        g[--a].push_back(--b);
+    }
+    col.assign(n, 0);
+    for(int i = 0; i < n; i++){
+        if(!col[i]){
+            if(!dfs(i)){
+                // cout << "IMPOSSIBLE" << endl;
+                return 0;
+            }
+        }
+    }
+    l_longest.assign(n, INT_MIN);
+    for(int i = n - 1; i >= 0; i--){
+        for(int v : g[order[i]]){
+            l_longest[order[i]] = max(l_longest[order[i]], l_longest[v] + 1);
+        }
+    }
+    if(l_longest[n - 1] < 0){
+        cout << "IMPOSSIBLE";
+        return 0;
+    }
+    cout << l_longest[n - 1] << endl;
+    return 0;
+}
