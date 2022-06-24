dd180804597c5e429d5c8394de4e708ac52dd3de
Yash Gupta <yashbg7@gmail.com>
2022-06-25 01:02:55 +0530

    Complete dijkstra.cpp

--- Assignment4/dijkstra.cpp
+++ Assignment4/dijkstra.cpp
@@ -22,8 +22,9 @@ int main(){
     }
     vector<long long> d(n, LLONG_MAX);
     d[0] = 0;
-    vector<int> p(n, -1), nr(n, 0);
+    vector<int> p(n, -1), nr(n, 0), mine(n, INT_MAX), maxe(n, 0);
     nr[0] = 1;
+    mine[0] = maxe[0] = 0;
     set<pair<long long, int>> s;
     s.insert({0, 0});
     while(!s.empty()){
@@ -36,13 +37,17 @@ int main(){
                 d[v] = d[u] + w;
                 p[v] = u;
                 nr[v] = nr[u];
+                mine[v] = mine[u] + 1;
+                maxe[v] = maxe[u] + 1;
                 s.insert({d[v], v});
             }
             else if(d[u] + w == d[v]){
                 nr[v] = (nr[v] + nr[u]) % mod;
+                mine[v] = min(mine[v], mine[u] + 1);
+                maxe[v] = max(maxe[v], maxe[u] + 1);
             }
         }
     }
-    cout << d[n - 1] << ' ' << nr[n - 1] << endl;
+    cout << d[n - 1] << ' ' << nr[n - 1] << ' ' << mine[n - 1] << ' ' << maxe[n - 1] << endl;
     return 0;
 }
